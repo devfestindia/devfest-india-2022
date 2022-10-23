3c29ef9ad713c9dfcb76980a3060f75c09d4f772
himanshu007-creator <addyjeridiq@gmail.com>
2022-10-24 00:22:28 +0530

    fix: sorted events according to dates

--- src/views/FindAnEvent.vue
+++ src/views/FindAnEvent.vue
@@ -6,18 +6,17 @@
           <v-container fluid>
             <v-row align="start" justify="start">
               <v-col md="11" lg="11">
-                <p
-                  class="google-font gdg-heading mb-0 grey--text text--darken-2"
-                >
+                <p class="google-font gdg-heading mb-0 grey--text text--darken-2">
                   DevFests in India
                 </p>
                 <p class="my-0 mb-8 h1-subheading">
-                  Communities from each chapter, all over the India have gathered together to make India DevFest a success. Locate your nearest google developer group and join the event right here!
+                  Communities from each chapter, all over the India have gathered together to make India DevFest a
+                  success. Locate your nearest google developer group and join the event right here!
                 </p>
                 <v-container fluid class="ma-0 pa-0">
                   <v-row align="start" justify="start">
-                    <v-col md="3" sm="6" v-for="(item,index) in data" :key="index">
-                        <DevFestDialogeVue :item="item"/>
+                    <v-col md="3" sm="6" v-for="(item, index) in data" :key="index">
+                      <DevFestDialogeVue :item="item" />
                     </v-col>
                   </v-row>
                 </v-container>
@@ -40,7 +39,9 @@ export default {
     DevFestDialogeVue,
   },
   data: () => ({
-    data: devfestData
+    data: devfestData.sort(function (a, b) {
+      return new Date(b.StartingDate) - new Date(a.StartingDate);
+    }),
   }),
   created() {
     document.title = "DevFests in India | DevFest India 2022";
@@ -48,5 +49,7 @@ export default {
 };
 </script>
   
-  <style scoped></style>
+<style scoped>
+
+</style>
   
